ad620d4d8604d1da4db8d95725522b313f7860dc
Niklas Ostwald <niktjawalla922@gmail.com>
2022-09-04 14:02:37 +0200

    added search function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
+import React, { useEffect, useState } from "react";
+
+
 function App() {
+
+useEffect(()=>{
+  async function fetchData() {
+    const response = await fetch(
+      `https://fetch-me.vercel.app/api/shopping/items`
+    );
+    const json = await response.json();
+    setData(json.data)
+  }
+  
+fetchData();
+},[])
+
+
+  
+const [data, setData] = useState ([])
+const [searchItem, setSearchItem] = useState ([])
+const [filteredItems, setFilteredItems] = useState([]);
+const [savedItems, setSavedItems] = useState([])
+
+
+
   return (
+    <>
     <h1>Shopping List</h1>
+  
+    <h2>search</h2>
+    <input type = "text" placeholder = "input search" value = {searchItem} onChange = {(event)=>{
+      setSearchItem (event.target.value)
+      const items = data.filter((item) => item.name.de.includes(searchItem));
+      setFilteredItems(items);
+    }}></input>
+    <ul>
+    {filteredItems.map ((item)=> {
+      console.log(filteredItems)
+      return(
+        <li key = {item._id}>
+          <div>
+            <p>{item.name.de}</p>
+            <button onClick = {()=>{ 
+            setSavedItems(...item, filteredItems)
+            }}
+            >X
+            </button>
+          </div>
+        </li>
+      )
+    })}
+    </ul>
+    <ul>
+      {savedItems.map ((savedItem)=>{
+        return(
+          <li>{savedItem.name.de}</li>
+        )
+      })}
+    </ul>
+    </>
   );
+
+
 }
 
 export default App;
